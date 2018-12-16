bbd46e4e646bf7d89b08b3e25aa0c288aa78836c
mithilaZohora <44693392+mithilaZohora@users.noreply.github.com>
2018-12-16 20:15:09 +0600

    Update puzzleRect.c

--- puzzleRect.c
+++ puzzleRect.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-    int t,n,ti,ni,l[20000],b[20000],r[20000],u[20000],solvable=0,result,moveLeft=0,moveBottom=0,moveUp=0,moveRight=0, height1,height2;
+    int t,n,ti,ni,l[20000],b[20000],r[20000],u[20000],solvable=0,result,moveLeft=0,moveBottom=0,moveUp=0,moveRight=0, height1,height2,sumOfRect=0,rect;
 
 
 void bottomOrUp(){
@@ -41,10 +41,7 @@ void match(){
                 if(l[0]=l[ni+1]){
                     break;
                 }
-
-
-
-                }
+        }
                 //left match
         if(l[ni]==l[ni+1]){
                 while(b[0]!=b[ni+1]){
@@ -55,10 +52,21 @@ void match(){
                 if(b[0]=b[ni+1]){
                     break;
                 }
+        }
 
+        //neither left nor bottom matches
+        if(l[ni]!=l[ni+1] && b[ni]!=b[ni+1]){
+            while(l[0]!=l[ni+1]){
 
+                    leftOrRight();
 
                 }
+                while(b[0]!=b[ni+1]){
+
+                    bottomOrUp();
+
+                }
+        }
 
 
    }
@@ -77,14 +85,38 @@ void isSolvable(){
                 }
     }
 
-    if(solvable>1) {
+    if(solvable==n) {
          printf("Puzzle can be solved\n");
          match();
         result=moveRight+moveLeft+moveBottom+moveUp;
-            printf("Steps %d", result);
+            printf("Steps %d\n", result);
     } else{
-        printf("Puzzle can't be solved");
+        printf("Puzzle can't be solved\n");
     }
+
+}
+
+void rectOutOfRange(){
+
+    if(sumOfRect>1000000){
+        printf("Total number of Rectangles is out of Range\n");
+    }
+
+    printf("Number of Rectangles: %d", sumOfRect);
+}
+
+void pointRange(){
+
+        for(ni=0;ni<n;ni++){
+
+            if( ((l[ni] && b[ni] && r[ni] && u[ni])>=-109) && ((l[ni] && b[ni] && r[ni] && u[ni])<=109) ){
+                   isSolvable();
+
+            } else{
+                printf("Points out of Range\n");
+            }
+    }
+
 
 }
 
@@ -97,12 +129,20 @@ int main(){
 
     if(t>=1 && t<=200000){
             for(ti=0;ti<t;ti++){
+                    printf("\n************************************\n");
+                    printf("For the Test Case %d,\n",ti+1);
 
         printf("Enter the number of Rectangles: ");
         scanf("%d", &n);
 
+
+        sumOfRect=sumOfRect+n;
+
+
             if(n>=1 && n<=200000){
+
                 for(ni=0;ni<n;ni++){
+
                    printf("Enter the points of the rectangle %d: (sequentially left, bottom, right and up)\n", ni+1);
                   scanf("%d %d %d %d", &l[ni],&b[ni], &r[ni], &u[ni]);
 
@@ -110,15 +150,20 @@ int main(){
                 }
 
 
-        }
+        }   else{
+                printf("Number of Rectangle is out of Range\n");
             }
+            pointRange();
+        }
+
+
 
     } else{
-        printf("Test case out of Range");
+        printf("Test case out of Range\n");
     }
 
-    isSolvable();
+
+    rectOutOfRange();
 
     return 0;
 }
-
